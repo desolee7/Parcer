80db32a24b9a2d0814cfc134714e144805b85888
desolee7 <125908645+desolee7@users.noreply.github.com>
2024-05-11 15:27:03 +0300

    update

--- yandex parcer/my_parc.py
+++ yandex parcer/my_parc.py
@@ -3,24 +3,24 @@ from yandex_images_parser import Parser
 
 parser = Parser(headless=False)
 
-square_deer = parser.query_search(query="Przewalskium albirostris — Беломордый олень",
-                                 limit=200,
-                                 size=parser.size.medium,
-                                 orientation=parser.orientation.square)
 
-square_deer_url = square_deer[0]
-
-similar_deers = parser.image_search(url=square_deer_url,
-                                   limit=500)
-
-print(f"Przewalskium albirostris — Беломордый олень:\n{similar_deers}\n")
-
-print("Przewalskium albirostris — Беломордый олень:")
-for cat in similar_deers:
-    print(cat)
-
-output_dir = "./Семейство оленевые/Подсемейство Cervinae/Род Przewalskium"
+output_dir = "./чб/кабарга фотоохота ночь"
 make_directory(output_dir)
 
-save_images(square_deer, dir_path=output_dir, prefix="original_")
-save_images(similar_deers, dir_path=output_dir, prefix="similar_", number_images=True)
+square_deer = parser.query_search(query='кабарга ночная фотоохота',
+                                 limit=43,
+                                 color=parser.color.gray,
+                                 )
+for i in range (1, 43):
+    square_deer_url = square_deer[i]
+    similar_deers = parser.image_search(url=square_deer_url,
+                                    limit=3000)
+
+    print(f"Moschidae:\n{similar_deers}\n")
+
+    print("Moschidae:")
+
+    for kos in similar_deers:
+        print(kos)
+    save_images(square_deer, dir_path=output_dir, prefix="original_")
+    save_images(similar_deers, dir_path=output_dir, prefix="similar_", number_images=True)
